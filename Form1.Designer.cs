eb5a055028d5e497672e3d232a6d8a705052cda6
the-woose <yavuzsava@yahoo.com.tr>
2023-05-20 16:00:48 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace BTE202_LAB12
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button2 = new Button();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(107, 113);
+            button2.Name = "button2";
+            button2.Size = new Size(109, 52);
+            button2.TabIndex = 3;
+            button2.Text = "&Zar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(107, 55);
+            button1.Name = "button1";
+            button1.Size = new Size(109, 52);
+            button1.TabIndex = 2;
+            button1.Text = "&Sayaç";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(331, 215);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Yavuz Sava - 2210780022";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button2;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,22 @@
+namespace BTE202_LAB12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Form2 frm_sayac = new Form2();
+            //frm_sayac.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Form3 frm_zar = new Form3();
+            //frm_zar.ShowDialog();
+        }
+    }
+}
